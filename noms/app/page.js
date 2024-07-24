97c4604da7098209e037648ffb17b5bd4c27ba1f
Sam Ko <samk3180@gmail.com>
2024-07-23 19:53:25 -0700

    added Navbar

--- noms/app/page.js
+++ noms/app/page.js
@@ -1,18 +1,26 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import {Box, Typography} from "@mui/material";
+import Navbar from "./components/Navbar"
+import {Box, Container, Typography} from "@mui/material";
 import { Dancing_Script } from "next/font/google";
 
 const dancingScript = Dancing_Script({subsets: ['latin']})
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Box> 
-        <Typography sx={{fontFamily: dancingScript.style.fontFamily, fontSize: '150px'}}>
-          NOMS
-        </Typography>
+    <Container>
+      <Box left='0%' position={'fixed'} width={'100%'}>
+        <Navbar></Navbar>
       </Box>
-    </main>
+      <main className={styles.main}>
+        
+        <Box> 
+          <Typography sx={{fontFamily: dancingScript.style.fontFamily, fontSize: '150px'}}>
+            NOMS
+          </Typography>
+        </Box>
+      </main>
+    </Container>
+    
   );
 }
