15c4667d844a5c277841ebc63d53d67b115c7130
Sam Ko <samk3180@gmail.com>
2024-07-30 12:45:55 -0700

    installed formik

--- noms/app/create/page.js
+++ noms/app/create/page.js
@@ -2,31 +2,24 @@
 import Image from "next/image";
 import styles from "../page.module.css";
 import * as React from 'react';
-import Navbar from "../components/Navbar";
-import Navdrawer from "../components/Navdrawer";
+import Navbar from '../components/Navbar'
 import {Box, Container, Typography} from "@mui/material";
 import { Dancing_Script } from "next/font/google";
 
 const dancingScript = Dancing_Script({subsets: ['latin']})
 
 export default function Create() {
-  const [isNavdrawerOpen, setNavdrawerOpen] = React.useState(false)
-
-  const handleNavdrawerOpen = () => {
-    setNavdrawerOpen(!isNavdrawerOpen)
-  }
-
+  
   return (
     <Container>
       <Box left='0%' width={'100%'}>
-        <Navdrawer open={isNavdrawerOpen} setOpen={handleNavdrawerOpen}></Navdrawer>
-        <Navbar setOpen={handleNavdrawerOpen} position='fixed' sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}></Navbar>
-      </Box>    
-      <main className={styles.main}> 
+        <Navbar></Navbar>
+      </Box>
+      <main className={styles.main}>
         
-        <Box> 
-          <Typography sx={{fontFamily: dancingScript.style.fontFamily, fontSize: '150px'}}>
-            NOMS
+        <Box width='800px' sx={{backgroundColor: 'grey'}}> 
+          <Typography fontSize={'50px'}>
+            Create New Recipe
           </Typography>
         </Box>
       </main>
