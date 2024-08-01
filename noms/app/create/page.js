f5eac875c2c5608d86cc63f51c2e2ab7dbe541a1
Sam Ko <samk3180@gmail.com>
2024-08-01 01:30:00 -0700

    added formik for ingredients section in "create a nom"

--- noms/app/create/page.js
+++ noms/app/create/page.js
@@ -10,10 +10,22 @@ const units = ['g', 'mL']
 
 export default function Create() {
   const [addIngredientMode, setIngredientMode] = React.useState(false)
+  const [ingredients, setIngredients] = React.useState([{quantity: 2, unit: 'g', name: 'Salt'}])
+
+  const ingredientFormik = useFormik({
+    initialValues: {
+        quantity: '',
+        unit: '', 
+        name: ''
+    },
+    onSubmit: (values) => {
+        console.log('hi')
+    }
+  })
 
   const formik = useFormik({
     initialValues: {
-        title: ""
+        title: ''
     }
   })
 
@@ -28,31 +40,70 @@ export default function Create() {
       </Box>
       <main className={styles.main}>
         
-        <Box width='800px' sx={{}}> 
+        <Box width='800px' display={"flex"} flexDirection={'column'} sx={{gap: '10px'}}> 
           <Typography fontSize={'50px'}>
             Create A Nom
           </Typography>
           <Divider sx={{margin: '10px'}}></Divider>
           <Stack direction='row'>
             <Typography fontSize="25px">Name:</Typography>
-            <TextField variant="outlined" fullWidth></TextField>
+            <TextField variant="outlined" fullWidth sx={{margin: '5px'}}></TextField>
           </Stack>
           <Box>
             <Typography fontSize="25px">Ingredients:</Typography>
+            {ingredients.map((ingredient) => (
+                <Typography>{ingredient.quantity + ingredient.unit + ' ' + ingredient.name}</Typography>
+            ))}
             {addIngredientMode ? 
                 <Box display="flex">
-                    <Typography>Ingredient Name</Typography>
-                    <TextField></TextField>
+                    <Typography>Ingredient</Typography>
+                    <TextField 
+                        sx={{margin: '5px'}} 
+                        value={ingredientFormik.values.name}
+                        id="name"
+                        onChange={ingredientFormik.handleChange}>
+                    </TextField>
                     <Typography>Quantity</Typography>
-                    <TextField></TextField>
-                    <TextField select>
-                        {units.map((unit) => (
-                            <MenuItem key={unit} value={unit}>{unit}</MenuItem>
-                        ))}
+                    <TextField 
+                        sx={{margin: '5px'}} 
+                        value={ingredientFormik.values.quantity}
+                        id="quantity"
+                        onChange={ingredientFormik.handleChange}>
+                    </TextField>
+                    <Typography>Unit</Typography>
+                    <TextField 
+                        select 
+                        sx={{margin: '5px'}} 
+                        value={ingredientFormik.values.unit}
+                        id="unit"
+                        onChange={ingredientFormik.handleChange('unit')}>
+                            {units.map((unit) => (
+                                <MenuItem key={unit} value={unit}>{unit}</MenuItem>
+                            ))}
                     </TextField>
                 </Box> : 
-            <></>}
-            <Button onClick={() => handleIngredientMode()}>{addIngredientMode ? 'Confirm' : '+ Add Ingredient'}</Button>
+                <></>
+            }
+            {addIngredientMode ? 
+                <Box>
+                    <Button 
+                        onClick={() => {
+                            handleIngredientMode() 
+                            ingredientFormik.handleSubmit() 
+                            ingredientFormik.resetForm()}}
+                    >
+                        Confirm
+                    </Button>
+                    <Button 
+                        onClick={() => {
+                            handleIngredientMode()
+                            ingredientFormik.resetForm()}}
+                    >
+                        Cancel
+                    </Button>
+                </Box> : 
+                <Button onClick={() => handleIngredientMode()}>+ Add Ingredient</Button>
+            }
           </Box>
           <Box>
             <Typography fontSize="25px">Instructions:</Typography>
@@ -63,6 +114,7 @@ export default function Create() {
             <Typography fontSize='25px'>Notes:</Typography>
             <TextField variant="outlined" fullWidth></TextField>
           </Box>
+          <Button variant="filled" sx={{alignSelf: 'end', backgroundColor: 'lightblue'}}>Create</Button>
         </Box>
       </main>
     </Container>
