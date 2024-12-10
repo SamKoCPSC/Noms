e95c943fc3f5ecf0cb043236efd52a2da997c42b
Sam Ko <samk3180@gmail.com>
2024-12-10 00:57:16 -0800

    created lambda function for querying postgresql database

---
+++ Lambda/queryHandler.py
@@ -0,0 +1,71 @@
+import sys
+import logging
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import os
+import json
+
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+host = os.environ['HOST']
+DBname = os.environ['DB_NAME']
+port = os.environ['PORT']
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+connection = None
+
+def get_connection():
+    global connection
+    if connection is None or connection.closed:
+        try:
+            connection = psycopg2.connect(
+                dbname=DBname,
+                user=username,
+                password=password,
+                host=host,
+                port=port
+            )
+            logger.info("SUCCESS: Connection to RDS succeeded")
+        except Exception as e:
+            logger.error("FAILURE: Connection to RDS failed")
+            logger.error(e)
+            sys.exit(1)
+    return connection
+
+def queryHandler(event, context):
+    connection = get_connection()
+    cursor = connection.cursor(cursor_factory=RealDictCursor)
+    sql = event['queryStringParameters']['sql']
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        logger.info(f"Query Result: {result}")
+        connection.commit()
+        return {
+            'statusCode': 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            'body': json.dumps({
+                "result": result
+            }, default=str)
+        }
+    except Exception as e:
+        logger.error(e)
+        connection.rollback()
+        return {
+            'statusCode': 400,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            'body': json.dumps({
+                "status": "error",
+                "message": "Invalid SQL query",
+                "details": str(e)
+            })
+        }
+    finally:
+        if cursor:
+            cursor.close()
