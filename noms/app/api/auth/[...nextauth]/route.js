164ff6fd48f7f24230974366ef1ea9cae25503a3
Sam Ko <samk3180@gmail.com>
2025-02-01 00:56:44 -0800

    changed secret to nextauth_secret

--- noms/app/api/auth/[...nextauth]/route.js
+++ noms/app/api/auth/[...nextauth]/route.js
@@ -16,7 +16,7 @@ export const authOptions = {
               }
         }),
     ],
-    secret: process.env.SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
     callbacks: {
       async signIn({ user }) {
         try {
