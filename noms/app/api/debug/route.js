2c3be930ed6db2ad7d2acd3cd9e9a68097be0dc7
Sam Ko <samk3180@gmail.com>
2025-02-02 02:04:11 -0800

    removed debug api

--- noms/app/api/debug/route.js
+++ noms/app/api/debug/route.js
@@ -1,8 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    console.log("Environment Variables:", process.env);
-    return NextResponse.json({
-        apiUrl: process.env.LAMBDA_API_URL,
-    })
+    
 }
