4a1de9c77a4154b2f2106913a4343eef999f6cdf
Sam Ko <samk3180@gmail.com>
2025-02-02 01:43:36 -0800

    adjusted environment setup

--- noms/app/api/debug/route.js
+++ noms/app/api/debug/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
+    console.log("Environment Variables:", process.env);
     return NextResponse.json({
         apiUrl: process.env.LAMBDA_API_URL,
     })
