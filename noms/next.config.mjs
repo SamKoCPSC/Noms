e38b59417b8c4206b67a5e35eb3768982a017f24
Sam Ko <samk3180@gmail.com>
2025-02-02 02:27:58 -0800

    testing to see if removing next config affects deployment

--- noms/next.config.mjs
+++ noms/next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-        LAMBDA_API_KEY: process.env.LAMBDA_API_KEY,
-        LAMBDA_API_URL: process.env.LAMBDA_API_URL,
-        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-        NOMS_URL: process.env.NOMS_URL,
-        S3_ACCESS: process.env.S3_ACCESS,
-        S3_BUCKET: process.env.S3_BUCKET,
-        S3_REGION: process.env.S3_REGION,
-        S3_SECRET: process.env.S3_SECRET,
-      }, 
+    // env: {
+    //     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    //     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    //     LAMBDA_API_KEY: process.env.LAMBDA_API_KEY,
+    //     LAMBDA_API_URL: process.env.LAMBDA_API_URL,
+    //     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    //     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    //     NOMS_URL: process.env.NOMS_URL,
+    //     S3_ACCESS: process.env.S3_ACCESS,
+    //     S3_BUCKET: process.env.S3_BUCKET,
+    //     S3_REGION: process.env.S3_REGION,
+    //     S3_SECRET: process.env.S3_SECRET,
+    //   }, 
 };
 
 export default nextConfig;
