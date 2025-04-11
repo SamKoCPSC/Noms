bf73b3eca455b868303100a8962b047a9f23cc9c
Sam Ko <samk3180@gmail.com>
2025-04-11 02:28:49 -0700

    added included and excluded ingredients filter to search api

--- noms/app/api/search/route.js
+++ noms/app/api/search/route.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export async function GET(req, res) {
-    const name = `%${req.nextUrl.searchParams.get('name')}%`
-    const ingredients = ['%potato%', '%flour%']
+    const name = req.nextUrl.searchParams.get('name') ? `%${req.nextUrl.searchParams.get('name')}%` : '%%'
+    const includedIngredients = ['%flour%', '%sugar%']
+    const excludedIngredients = ['%butter%']
     return axios.post(
         process.env.LAMBDA_API_URL,
         {
@@ -37,12 +38,17 @@ export async function GET(req, res) {
                 LEFT JOIN users u ON r.userid = u.id
                 LEFT JOIN recipe_ingredients ON r.id = ri.recipeid
                 LEFT JOIN ingredients ON ri.ingredientid = i.id
-                WHERE r.name ILIKE %s and LOWER(i.name) LIKE ANY (ARRAY[
-				  ${'%s,'.repeat(ingredients.length).slice(0, -1)}
-				])
+                WHERE r.name ILIKE %s and i.name ILIKE ANY (ARRAY[${'%s,'.repeat(includedIngredients.length).slice(0, -1)}]) 
+                AND r.id NOT IN (
+                    SELECT r2.id
+                    FROM recipes r2
+                    JOIN recipe_ingredients ri2 ON r2.id = ri2.recipeid
+                    JOIN ingredients i2 ON ri2.ingredientid = i2.id
+                    WHERE i2.name ILIKE ANY (ARRAY[${'%s,'.repeat(excludedIngredients.length).slice(0, -1)}])
+                )
                 GROUP BY r.id, u.name;
             `,
-            values: [name].concat(ingredients)
+            values: [name].concat(includedIngredients).concat(excludedIngredients)
         },
         {
             headers: {
