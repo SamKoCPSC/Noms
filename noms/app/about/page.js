f49c8a2c4f57cfe111166ba9460672e6368ba0ac
Sam Ko <samk3180@gmail.com>
2025-08-22 01:14:43 -0700

    updated about page

--- noms/app/about/page.js
+++ noms/app/about/page.js
@@ -16,12 +16,14 @@ export default function About() {
             <Box sx={{marginX: '20px'}}>
                 <Typography sx={{fontSize: '1.5rem'}}>
                     Hi! I'm <b>Sam</b>, the creator of this app.<br/><br/> 
-                    I created <b>Noms</b> because as a food and cooking hobbyist I often found myself needing
-                    a place to store and keep track of my recipes. I used to write down my recipes using Google Docs, but as I developed more recipes over time
-                    I found that I would lose track of changes, and the reasoning behind why I made certain changes. I also found that I didn't have a convenient 
-                    way to share recipes, include images of my recipes, or scale my recipes to different sizes.<br/><br/>
+                    I created <b>Noms</b> because as a food and cooking hobbyist I often found myself encountering the same problems repeatedly. The primary problem was I
+                    wanted a convenient place to store and keep track of my recipes, including images of the product, the ingredients, and list of instructions. 
+                    I used to write down my recipes using Google Docs, but as I developed more recipes over time I found that I would lose track of changes, and the 
+                    reasoning behind why I made certain changes.<br/><br/> 
+                    Overtime I also discovered other problems and I wished that there would be an easier way to scale recipes accordingly, organize 
+                    similar recipes together, figure out what I could make based on the ingredients I had available or figure out what I needed to buy for a particular recipe<br/><br/>
                     And so, I decided to create my own solution. With <b>Noms</b> anyone can create an account, and start archiving, developing, and sharing recipes. 
-                    Once a recipe is created, a page for that recipe will be automatically generated. New versions and variations of recipes can be created based on 
+                    Once a recipe is created, a stylized page for that recipe will be automatically generated. New versions and variations of recipes can be created based on 
                     older recipes, and overtime, a recipe may develop into a tree of recipe iterations.
                 </Typography>
             </Box>
